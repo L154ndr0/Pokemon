4b266fa4035657d64c71fedcca8a6a1707c1e083
lisandro avila <lisandroam35@gmai.com>
2024-05-18 12:43:10 -0400

    estilo-2

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import PokemonPicture from './components/PokemonPicture.vue';
   const showPokemon = ref<boolean>(false)
   const showAnswer = ref<boolean>(false)
   const message = ref<string>()
+  const streack = ref<number>(0)
 
 
   const mixPokemonArray =async()=>{
@@ -20,16 +21,27 @@ import PokemonPicture from './components/PokemonPicture.vue';
     console.log(pokemon.value)
     console.log(pokemonArr.value)
   }
+  
 
   const checkAnswer =(selectedId: number)=>{
     showPokemon.value = true
     showAnswer.value = true
 
     if(selectedId === pokemon.value?.id){
+      streack.value++
       message.value = `Correcto era un ${pokemon.value.name}`
     }
     else{
-      message.value = `Cuerniao, eso es un ${pokemon.value?.name}`
+      streack.value = 0
+      const motivationalMessage = [
+      `Cuerniao, eso es un ${pokemon.value?.name}`,
+      `por no saber q eso es un ${pokemon.value?.name} te montan cacho`,
+      `si lo supieras q eso es un ${pokemon.value?.name} tu novia no estuviera encima de mi`,
+      `Por no saber q eso es un ${pokemon.value?.name} llene a tu mama de leche, cachuo`,
+      `Por no saber q eso es un ${pokemon.value?.name} hice un trio con tu mama y tu novia, gay`,
+    ]  
+    const randomInt = Math.floor(Math.random() *motivationalMessage.length)
+    message.value = motivationalMessage[randomInt]
     }
   } 
 
@@ -48,6 +60,12 @@ mixPokemonArray()
 <template>
   <div class="m-12">
     <PokemonPicture :show-pokemon="showPokemon" v-if="pokemon" :pokemon-id="pokemon.id" />
+    <div class="flex items-center flex-col text-4xl font-bold mt-12">
+      <p> {{ streack }} </p>
+      <p v-if="streack == 10">Las llevas loca</p>
+
+    </div>
+
     <PokemonOptions @selection-pokemon="checkAnswer" :pokemons="pokemonArr" />
   </div>
   <div v-if="showAnswer" class="flex flex-col text-center space-y-6">
