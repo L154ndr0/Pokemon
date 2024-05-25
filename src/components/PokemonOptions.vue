44c9fc4bcb48433dccd3b1e550df4cc76ef07162
lisandro avila <lisandroam35@gmai.com>
2024-05-25 09:49:10 -0400

    estilo-3

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="grid grid-cols-2 mx-32 gap-12 mt-6 space-y-1">
         <button @click="$emit('selectionPokemon',pokemon.id)" class="text-3xl border-4 p-4 rounded-2xl" v-for="pokemon in pokemons"
-            :key="pokemon.id">
+            :key="pokemon.id"
+            :disabled="showPokemon == true || showAnswer == true">
                 {{ pokemon.name }}
         </button>
     </div>
@@ -12,6 +13,8 @@
 
     defineProps<{
         pokemons: Pokemon[]
+        showPokemon: boolean,
+        showAnswer: boolean,
     }>()
 
 </script>
